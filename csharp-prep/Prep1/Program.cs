cf63e668f4215b6109cbc46fddbcb239587be7cd
evedatadiva <evelin.flores@ucsp.edu.pe>
2024-04-27 06:41:22 -0500

    up

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("What is your first name? ");
+        string name = Console.ReadLine();
+
+        Console.WriteLine("What is your last name? ");
+        string last_name = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {name}, {name} {last_name}.");
+    
     }
 }
