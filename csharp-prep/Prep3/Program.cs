e134f400b8e03df5b7a91bc93664b7ec813b9056
evedatadiva <evelin.flores@ucsp.edu.pe>
2024-04-27 22:41:31 -0500

    listo

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random random = new Random();
+        int magicNumber = random.Next(1, 20);
+
+        Console.WriteLine("Welcome to Guess My Number Game!!!");
+        Console.WriteLine("I have picked a magic number between 1 and 20.");
+        Console.WriteLine("I hope you enjoy it!!!"); 
+        
+        int guess = 0;
+
+        while (true)
+        {
+            Console.Write("What is your guess?: ");
+            string input = Console.ReadLine();
+
+            if (int.TryParse(input, out guess))
+            {
+                if (guess == magicNumber)
+                {
+                    Console.WriteLine("You guessed it!");
+                    break; 
+                }
+                else if (guess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else
+                {
+                    Console.WriteLine("Lower");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a valid number.");
+            }
+        }
     }
 }
